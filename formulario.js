d2b2e5831f6c48f2c5ed44996975361da78279e0
segurancadainformacao <127057402+segurancadainformacao@users.noreply.github.com>
2023-03-07 23:27:13 -0300

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,10 @@
+function enviar() {
+
+
+
+    { var nome = document.getElementById("nome").value;
+
+    alert('Obrigado Sr(a) ' + nome + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
