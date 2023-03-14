14cb3b6063cebaf166544d8434e4ecbf94219e05
segurancadainformacao <127057402+segurancadainformacao@users.noreply.github.com>
2023-03-13 23:16:24 -0300

    Add files via upload

---
+++ iwdois/Site/formulario.js
@@ -0,0 +1,13 @@
+function gets(button) {
+
+    const v = button.value //or button id
+
+    switch(v){
+        case 1:alert("Pagina 1")
+        break
+        case 2:alert("pagina 2")
+        break
+        case 3:alert("Pagina 3")
+        break
+    }
+}
